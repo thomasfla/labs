ccc1f3acdafbb0fa530c5730264c49c44c867d3a
odri <odri@furano.laas.fr>
2020-10-21 14:06:14 +0200

    set the feet references outside of the inverse kinematic compute function, add viewer call, set feet references to half sitting

--- solo12InvKin.py
+++ solo12InvKin.py
@@ -10,39 +10,18 @@ from example_robot_data import load
 class Solo12InvKin:
     def __init__(self):
         self.robot = load('solo12')
-        #Inputs
-        self.dt = 0.001
-        self.feet_position_ref =     [np.array([0.1946,   0.16891, 0.0191028]),np.array([0.1946,  -0.16891, 0.0191028]),np.array([-0.1946,   0.16891, 0.0191028]),np.array([-0.1946,  -0.16891, 0.1191028])]
+
+        #Inputs to be modified bu the user before calling .compute
+        self.feet_position_ref =     [np.array([0.1946,   0.16891, 0.0191028]),np.array([0.1946,  -0.16891, 0.0191028]),np.array([-0.1946,   0.16891, 0.0191028]),np.array([-0.1946,  -0.16891, 0.0191028])]
         self.feet_velocity_ref =     [np.array([0,0,0]), np.array([0,0,0]), np.array([0,0,0]), np.array([0,0,0])]
         self.feet_acceleration_ref = [np.array([0,0,0]), np.array([0,0,0]), np.array([0,0,0]), np.array([0,0,0])]
-        self.flag_in_contact = np.array([1,0,0,0])
+        self.flag_in_contact = np.array([0,1,0,1])
         self.base_orientation_ref = pin.utils.rpyToMatrix(0,0,np.pi/6) 
         self.base_angularvelocity_ref = np.array([0,0,0])
         self.base_angularacceleration_ref = np.array([0,0,0])
         self.base_position_ref = np.array([0,0,0.235])
         self.base_linearvelocity_ref = np.array([0,0,0])
         self.base_linearacceleration_ref = np.array([0,0,0])
-
-
-        self.feet_position_ref = [
-            lambda t: 0*np.array([0.1946,   0.16891, 0.0191028]),
-            lambda t: 0.0*np.array([0.1946,  -0.16891, 0.0191028]),
-            lambda t: np.array([-0.1946,   0.16891, (1-np.cos(10*t))*.1+0.0191028]),
-            #np.array([-0.1946,   0.16891, 0.1191028]),
-            lambda t: np.array([-0.1946,  -0.16891, 0.1191028])
-        ]
-        self.feet_velocity_ref = [
-            lambda t: np.array([0,0,0.]),
-            lambda t: np.array([0,0,0.]),
-            lambda t: np.array([0,0, np.sin(10*t)]),
-            lambda t: np.array([0,0,0.]),
-        ]
-        self.feet_acceleration_ref = [
-            lambda t: np.array([0,0,0.]),
-            lambda t: np.array([0,0,0.]),
-            lambda t: np.array([0,0, 10*np.cos(10*t)]),
-            lambda t: np.array([0,0,0.]),
-        ]
 
         self.Kp_base_orientation = 100
         self.Kd_base_orientation = 2*np.sqrt(self.Kp_base_orientation)
@@ -52,7 +31,6 @@ class Solo12InvKin:
 
         self.Kp_flyingfeet = 100
         self.Kd_flyingfeet = 2*np.sqrt(self.Kp_flyingfeet)
-
 
         #Get frame IDs
         FL_FOOT_ID = self.robot.model.getFrameId('FL_FOOT')
@@ -70,7 +48,6 @@ class Solo12InvKin:
                 Sinv = S/(S**2+damping**2)
             return (V.T*Sinv)@U.T
 
-
         hpos = []
         hbas = []
         self.rmodel= self.robot.model
@@ -86,11 +63,7 @@ class Solo12InvKin:
             Sinv = S/(S**2+damping**2)
         return (V.T*Sinv)@U.T
 
-
-
-
     def compute(self,q,dq):
-        self.i+=1
         ### FEET
         Jfeet = []
         afeet = []
@@ -102,12 +75,12 @@ class Solo12InvKin:
             idx = int(self.foot_ids[i_ee])
             pos = self.rdata.oMf[idx].translation
             nu = pin.getFrameVelocity(self.rmodel,self.rdata,idx,pin.LOCAL_WORLD_ALIGNED)
-            ref = self.feet_position_ref[i_ee](self.i*self.dt)
-            vref = self.feet_velocity_ref[i_ee](self.i*self.dt)
-            aref = self.feet_acceleration_ref[i_ee](self.i*self.dt)
+            ref = self.feet_position_ref[i_ee]
+            vref = self.feet_velocity_ref[i_ee]
+            aref = self.feet_acceleration_ref[i_ee]
             
             J1 = pin.computeFrameJacobian(self.robot.model,self.robot.data,q,idx,pin.LOCAL_WORLD_ALIGNED)[:3]
-            acc1 = -self.Kp_flyingfeet*(pos-ref) - Kd_flyingfeet*(nu.linear-vref) + aref
+            acc1 = -self.Kp_flyingfeet*(pos-ref) - self.Kd_flyingfeet*(nu.linear-vref) + aref
             if self.flag_in_contact[i_ee] :
                 acc1 *= 0 # In contact = no feedback
             drift1 = np.zeros(3)
@@ -154,13 +127,45 @@ class Solo12InvKin:
 
         return ddq
 
+
+
+
 print("test")
 dt = 0.001
 invKin = Solo12InvKin() 
 q = invKin.robot.q0.copy()
 dq = invKin.robot.v0.copy()
 
-for i in range(1000):
+if USE_VIEWER:
+    invKin.robot.initViewer(loadModel=True)
+    invKin.robot.viewer.gui.setRefreshIsSynchronous(False)
+    #invKin.robot.display(q)
+
+for i in range(10000):
+    if USE_VIEWER:
+        invKin.robot.display(q)
+
+    t=i*dt
+
+    #set the references
+    invKin.feet_position_ref = [
+            np.array([0.1946,   0.16891, 0.0191028]),
+            np.array([0.1946,  -0.16891, 0.0191028]),
+            np.array([-0.1946,   0.16891, (1-np.cos(10*t))*.1+0.0191028]),
+            np.array([-0.1946,  -0.16891, 0.0191028])]
+
+    invKin.feet_velocity_ref = [
+            np.array([0,0,0.]),
+            np.array([0,0,0.]),
+            np.array([0,0, np.sin(10*t)]),
+            np.array([0,0,0.])]
+            
+    invKin.feet_acceleration_ref = [
+            np.array([0,0,0.]),
+            np.array([0,0,0.]),
+            np.array([0,0, 10*np.cos(10*t)]),
+            np.array([0,0,0.])]
+
     ddq = invKin.compute(q,dq)
     dq=dq+dt*ddq
     q=pin.integrate(invKin.robot.model,q,dq*dt)
