9afea0e8ff10496cd48938842832b1e48d2a75d9
odri <odri@furano.laas.fr>
2020-10-22 15:26:26 +0200

    replace np.cross with a cross3, add computation of q_cmd and dq_cmd for joint impedance control

--- solo12InvKin.py
+++ solo12InvKin.py
@@ -48,8 +48,6 @@ class Solo12InvKin:
                 Sinv = S/(S**2+damping**2)
             return (V.T*Sinv)@U.T
 
-        hpos = []
-        hbas = []
         self.rmodel= self.robot.model
         self.rdata= self.robot.data
         self.i=0
@@ -63,11 +61,19 @@ class Solo12InvKin:
             Sinv = S/(S**2+damping**2)
         return (V.T*Sinv)@U.T
 
+
+    def cross3(self, left, right):
+        """Numpy is inefficient for this"""
+        return np.array([left[1] * right[2] - left[2] * right[1],
+                         left[2] * right[0] - left[0] * right[2],
+                         left[0] * right[1] - left[1] * right[0]])
+
     def compute(self,q,dq):
         ### FEET
         Jfeet = []
         afeet = []
-        pfeet = []
+        pfeet_err = []
+        vfeet_ref = []
         pin.forwardKinematics(self.rmodel,self.rdata,q,dq,np.zeros(self.rmodel.nv))
         pin.updateFramePlacements(self.rmodel,self.rdata)
 
@@ -80,31 +86,34 @@ class Solo12InvKin:
             aref = self.feet_acceleration_ref[i_ee]
             
             J1 = pin.computeFrameJacobian(self.robot.model,self.robot.data,q,idx,pin.LOCAL_WORLD_ALIGNED)[:3]
+            e1 = ref-pos
             acc1 = -self.Kp_flyingfeet*(pos-ref) - self.Kd_flyingfeet*(nu.linear-vref) + aref
             if self.flag_in_contact[i_ee] :
                 acc1 *= 0 # In contact = no feedback
             drift1 = np.zeros(3)
             drift1 += pin.getFrameAcceleration(self.rmodel,self.rdata,idx,pin.LOCAL_WORLD_ALIGNED).linear
-            drift1 += np.cross(nu.angular,nu.linear)
+            drift1 += self.cross3(nu.angular,nu.linear)
             acc1 -= drift1
 
             Jfeet.append(J1)
             afeet.append(acc1)
-            pfeet.append(pos)
+
+            pfeet_err.append(e1)
+            vfeet_ref.append(vref)
 
         ### BASE POSITION
         idx = self.BASE_ID
-
         pos = self.rdata.oMf[idx].translation
         nu = pin.getFrameVelocity(self.rmodel,self.rdata,idx,pin.LOCAL_WORLD_ALIGNED)
         ref = self.base_position_ref
         Jbasis = pin.computeFrameJacobian(self.robot.model,self.robot.data,q,idx,pin.LOCAL_WORLD_ALIGNED)[:3]
+        e_basispos = ref - pos
         accbasis = -self.Kp_base_position*(pos-ref) - self.Kd_base_position*nu.linear
         drift = np.zeros(3)
         drift += pin.getFrameAcceleration(self.rmodel,self.rdata,idx,pin.LOCAL_WORLD_ALIGNED).linear
-        drift += np.cross(nu.angular,nu.linear)
+        drift += self.cross3(nu.angular,nu.linear)
         accbasis -= drift
-        
+
         ### BASE ROTATION
         idx = self.BASE_ID
 
@@ -112,19 +121,24 @@ class Solo12InvKin:
         nu = pin.getFrameVelocity(self.rmodel,self.rdata,idx,pin.LOCAL_WORLD_ALIGNED)
         rotref = self.base_orientation_ref
         Jwbasis = pin.computeFrameJacobian(self.robot.model,self.robot.data,q,idx,pin.LOCAL_WORLD_ALIGNED)[3:]
+        e_basisrot = -rotref @ pin.log3(rotref.T@rot)
         accwbasis = -self.Kp_base_orientation * rotref @ pin.log3(rotref.T@rot) - self.Kd_base_orientation*nu.angular
         drift = np.zeros(3)
         drift += pin.getFrameAcceleration(self.rmodel,self.rdata,idx,pin.LOCAL_WORLD_ALIGNED).angular
         accwbasis -= drift
 
-        pinv = np.linalg.pinv
-        inv = np.linalg.inv
-
         J = np.vstack(Jfeet+[Jbasis,Jwbasis])
         acc = np.concatenate(afeet+[accbasis,accwbasis])
         
-        ddq = self.dinv(J) @ acc
+        x_err = np.concatenate(pfeet_err+[e_basispos,e_basisrot])
+        dx_ref = np.concatenate(vfeet_ref+[self.base_linearvelocity_ref,self.base_angularvelocity_ref])
 
+        invJ = self.dinv(J) #or np.linalg.inv(J) since full rank
+
+        ddq = invJ @ acc
+        self.q_cmd = pin.integrate(self.robot.model,q,invJ @ x_err)
+        self.dq_cmd = invJ @ dx_ref
+        
         return ddq
 
 
@@ -139,14 +153,13 @@ dq = invKin.robot.v0.copy()
 if USE_VIEWER:
     invKin.robot.initViewer(loadModel=True)
     invKin.robot.viewer.gui.setRefreshIsSynchronous(False)
+
+
     #invKin.robot.display(q)
 
-for i in range(10000):
-    if USE_VIEWER:
-        invKin.robot.display(q)
 
+for i in range(1000):
     t=i*dt
-
     #set the references
     invKin.feet_position_ref = [
             np.array([0.1946,   0.16891, 0.0191028]),
@@ -159,7 +172,7 @@ for i in range(10000):
             np.array([0,0,0.]),
             np.array([0,0, np.sin(10*t)]),
             np.array([0,0,0.])]
-            
+
     invKin.feet_acceleration_ref = [
             np.array([0,0,0.]),
             np.array([0,0,0.]),
@@ -169,4 +182,7 @@ for i in range(10000):
     ddq = invKin.compute(q,dq)
     dq=dq+dt*ddq
     q=pin.integrate(invKin.robot.model,q,dq*dt)
-    print (q)
+    
+    if USE_VIEWER:
+        invKin.robot.display(q)
+    #print (q)
