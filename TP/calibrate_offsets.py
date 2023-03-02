f78bff70634ad605fadc283fd99626dfb1e44967
Thomas Flayols <thomas.flayols@gmail.com>
2023-03-02 13:42:56 +0100

    restrict offsets to [-pi:pi]

--- TP/calibrate_offsets.py
+++ TP/calibrate_offsets.py
@@ -4,9 +4,9 @@ import time
 from odri_spi_rpi import SPIuDriver
 import time
 dt = 0.001
-ud = SPIuDriver(absolutePositionMode=True, offsets=[-2.5965261364443655,-0.2456724909824985])
-ud.goto(0,0)
-ud.goto(0,0)
+ud = SPIuDriver(absolutePositionMode=True, offsets=[0,0])
+ud.goto(0,0) 
+ud.goto(0,0) # To hold the motor steady
 ud.transfer()
 N=30000 #30 seconds
 t = time.perf_counter()
@@ -14,7 +14,9 @@ for i in range(N):
     ud.refCurrent0 = 0;
     ud.refCurrent1 = 0;
     if (i%10==0):
-        print(f"offsets=[{-ud.position0},{-ud.position1}]")
+        offset0 = ((ud.position0+pi)%(2*pi))-pi # result in [-pi ; pi]
+        offset1 = ((ud.position1+pi)%(2*pi))-pi 
+        print(f"offsets=[{-offset0},{-offset1}]")
     ud.transfer() #transfer
     #wait for next control cycle
     t +=dt
